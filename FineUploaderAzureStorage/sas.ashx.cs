482a99047b63e24a1982913d743abacd860cd0b2
hanfei <hanfei2013hello@163.com>
2016-10-20 17:54:31 +0800

    Update sas.ashx.cs

--- FineUploaderAzureStorage/sas.ashx.cs
+++ FineUploaderAzureStorage/sas.ashx.cs
@@ -23,7 +23,7 @@ namespace FineUploaderTest
             var verb = context.Request.QueryString.Get("_method");
 
 
-            var accountAndKey = new StorageCredentials("fehanstoragetest", "d5qSSNsS1ic3RuLCJ52p8KMP+GyrtIF4S8TftjN51ndd7OuIhMWlaunvoBbNJ5/ISW3nD5lfagVOsVopqMm4lA==");
+            var accountAndKey = new StorageCredentials("accountname", "accountkey");
 
             CloudBlockBlob blob = new CloudBlockBlob(new Uri(blobUri), accountAndKey);
 
@@ -40,52 +40,6 @@ namespace FineUploaderTest
 
             });
 
-
-            //if (context.Request.HttpMethod == "GET")
-
-            //{
-
-            //    var blobUri = context.Request.QueryString.Get("bloburi");
-
-            //    var verb = context.Request.QueryString.Get("_method");
-
-
-
-            //    var sas = getSasForBlob(accountAndKey, blobUri, verb);
-
-
-
-            //    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(sas);
-
-            //    //context.Response.con.ContentLength64 = buffer.Length;
-
-            //    //System.IO.Stream output = response.OutputStream;
-
-            //    //output.Write(buffer, 0, buffer.Length);
-
-            //    //output.Close();
-
-            //}
-
-            //else if (context.Request.HttpMethod == "POST")
-
-            //{
-
-            //    context.Response.StatusCode = 200;
-
-            //    // TODO insert uploadSuccess handling logic here
-
-            //    context.Response.Close();
-
-            //}
-
-            //else
-
-            //{
-
-            //    context.Response.StatusCode = 405;
-
-            //}
 
             context.Response.ContentType = "text/plain";
             context.Response.Write(string.Format(CultureInfo.InvariantCulture, "{0}{1}", blob.Uri, sas));
